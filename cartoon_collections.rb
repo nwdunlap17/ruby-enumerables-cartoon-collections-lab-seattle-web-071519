a6e4850d04ac11b7280073b723825fd9d79aa0a6
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 19:59:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,15 +4,15 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet{(cheer)}
+  cheer.collect{|x| x.capatalize}
+end
+
+def long_planeteer_calls(array)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
