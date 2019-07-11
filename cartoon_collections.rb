1ec4f27a37550848d15515c5d5b783951b3df56d
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 20:07:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(cheer)
-  cheer = cheer.collect { |x| x + "!" } 
+  cheer = cheer.collect { |x| x.capitalize + "!" } 
   return cheer
 end
 
